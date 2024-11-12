454a7a63144b1aacfa4a49392aca51e03b4a4682
Josue Morales <dev.carlos.morales@gmail.com>
2024-11-12 18:11:00 -0500

    add control value accessor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,39 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { InputAgeComponent } from './input-age.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, InputAgeComponent],
   styleUrl: './app.component.css',
   template: `
     <form class="form" [formGroup]="form" (ngSubmit)="save()">
       <input placeholder="Tu nombre" type="text" formControlName="name" />
+      <input-age formControlName="age" />
       <button type="submit">Guardar</button>
     </form>
+
+    <div>
+      <p>Value: {{ ageControl?.value }}</p>
+    </div>
   `,
 })
 export class AppComponent {
   private _formBuilder = inject(FormBuilder);
 
+  get ageControl() {
+    return this.form.get('age');
+  }
+
   form = this._formBuilder.group({
     name: this._formBuilder.control(''),
+    age: this._formBuilder.control('30'),
   });
+
+  constructor() {
+    this.ageControl?.disable();
+  }
 
   save() {
     console.log(this.form.value);
--- src/app/input-age.component.ts
+++ src/app/input-age.component.ts
@@ -1,4 +1,19 @@
-import { Component, input, output } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  forwardRef,
+  input,
+  output,
+  signal,
+  viewChild,
+} from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+
+const VALUE_ACCESSOR = {
+  provide: NG_VALUE_ACCESSOR,
+  useExisting: forwardRef(() => InputAgeComponent),
+  multi: true,
+};
 
 @Component({
   standalone: true,
@@ -8,8 +23,8 @@ import { Component, input, output } from '@angular/core';
       type="text"
       placeholder="Tu edad"
       (input)="_onInput($event)"
+      (blur)="_onBlur()"
       [disabled]="disabled()"
-      [value]="value()"
       #input
     />
   `,
@@ -23,18 +38,44 @@ import { Component, input, output } from '@angular/core';
         opacity: 0.5;
     }
   `,
+  providers: [VALUE_ACCESSOR],
 })
-export class InputAgeComponent {
-  disabled = input<boolean>(false);
+export class InputAgeComponent implements ControlValueAccessor {
+  disabled = signal<boolean>(false);
 
-  value = input<string>('');
+  value = signal<string>('');
 
-  changeValue = output<string>();
+  inputElement = viewChild.required<ElementRef<HTMLInputElement>>('input');
+
+  onChange: (value: string) => void = () => {};
+
+  onTouched: () => void = () => {};
 
   _onInput(event: Event) {
     const target = event.target as HTMLInputElement;
     let value = target.value;
 
-    this.changeValue.emit(value);
+    this.onChange(value);
+  }
+
+  _onBlur() {
+    this.onTouched();
+  }
+
+  writeValue(age: string): void {
+    this.value.set(age);
+    this.inputElement().nativeElement.value = age;
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState?(isDisabled: boolean): void {
+    this.disabled.set(isDisabled);
   }
 }
